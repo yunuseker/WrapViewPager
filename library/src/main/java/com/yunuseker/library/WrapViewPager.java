a1c0d24250765d5dd782c9851ea00e39d40f7bba
yunuseker <younuseker@gmail.com>
2018-05-07 14:36:08 +0300

    Initial commit

---
+++ library/src/main/java/com/yunuseker/library/WrapViewPager.java
@@ -0,0 +1,39 @@
+package com.yunuseker.library;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class WrapViewPager extends ViewPager {
+
+    public WrapViewPager(Context context) {
+        super(context);
+    }
+
+    public WrapViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int mode = MeasureSpec.getMode(heightMeasureSpec);
+        if (mode == MeasureSpec.UNSPECIFIED || mode == MeasureSpec.AT_MOST) {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+            int height = 0;
+            for (int i = 0; i < getChildCount(); i++) {
+                View child = getChildAt(i);
+                child.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+                int childMeasuredHeight = child.getMeasuredHeight();
+                if (childMeasuredHeight > height) {
+                    height = childMeasuredHeight;
+                }
+            }
+            heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
+        }
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+}
